4d98f36c6d2a26df9366df970450c78fb29db93a
hariprasadkool <hariprasad.g@techpearl.com>
2018-03-07 13:07:35 +0530

    using interface to keep(instance of article)Article creation verysimple

--- reddit-clone/src/app/article.service.ts
+++ reddit-clone/src/app/article.service.ts
@@ -45,13 +45,9 @@ export class ArticleService {
         .then( json => json.articles )
         .then( articles => {
           console.log('json->', articles);
-          const list = articles.map(article => new Article(
-            article.title,
-            article.description,
-            article.urlToImage
-          ));
-          console.log('json->', list);
-          return list;
+          return articles.map(article => Article.fromJSON(article));
+          // console.log('json->', list);
+          // return list;
         })
         .catch(err => {
           console.log(err);
--- reddit-clone/src/app/article.ts
+++ reddit-clone/src/app/article.ts
@@ -1,6 +1,30 @@
+
+interface ArticleJSON {
+  title: string;
+  url: string;
+  votes: number;
+  publishedAt: string;
+  description: string;
+  author: string;
+  urlToImage: string;
+}
+
+
+
 export class Article {
 
     public publishedAt: Date;
+
+    // Article.fromJSON()
+    static fromJSON(json: ArticleJSON): Article {
+      let article = Object.create(Article.prototype);
+      return Object.assign(article, json, {
+        votes: json.votes ? json.votes : 0,
+        imageUrl: json.urlToImage,
+        publishedAt: json.publishedAt ? new Date(json.publishedAt) : new Date()
+      });
+    }
+
     constructor(
       public title: string,
       public description: string,
